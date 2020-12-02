f10b3d37a2d19e5e8a90e92ce9fd9bee9d09ff9c
camthegeek <cam@camthegeek.net>
2020-12-02 09:35:54 -0500

    [web] improved design on header/navigation

--- web/src/Components/Header.jsx
+++ web/src/Components/Header.jsx
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 import Search from './Search';
+import { Navbar, NavbarBrand, Nav } from 'react-bootstrap';
 
 class Header extends Component {
     render() {
         return (
-            <section className="header">
-                <a href="/">
-                    <h1>ScPrime Explorer</h1>
-                </a>
+            <Navbar bg="light" expand="lg">
+                <NavbarBrand href="/">ScPrime Explorer</NavbarBrand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Dashboard</Nav.Link>
+                        <Nav.Link href="/storage">Storage</Nav.Link>
+                        <Nav.Link href="/mining">Mining</Nav.Link>
+                        <Nav.Link href="/tokenomics">Tokenomics</Nav.Link>
+                    </Nav>
                 <Search/>
-            </section>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
