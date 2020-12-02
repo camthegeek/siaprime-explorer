47956c203e19f234bcbd14f77a8da0d686333401
camthegeek <cam@camthegeek.net>
2020-12-02 14:36:21 -0500

    [web] pass states from app to where they need to go via routing

--- web/src/Components/Container.jsx
+++ web/src/Components/Container.jsx
@@ -1,15 +1,14 @@
 import React, {Component} from "react";
 import { Switch, Route } from 'react-router-dom';
-
 import Home from '../Pages/Home';
 import BlockInfo from '../Pages/Blocks';
 import NotFound from '../Pages/404';
 
-class Container extends Component {
+class Container extends Component {        
     render() {
-        return(
+        return(            
             <Switch> {/* The Switch decides which component to show based on the current URL.*/}
-                <Route exact path='/' component={Home}></Route>
+                <Route exact path='/' render={()=><Home morestates={this.props.data}/>}/>
                 <Route exact path='/block/:id' component={BlockInfo}></Route>
                 <Route component={NotFound} />
             </Switch>
