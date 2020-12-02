5c4bc6bb12f95187e198d563c6089b8cdcb547d9
camthegeek <cam@camthegeek.net>
2020-12-02 09:57:16 -0500

    [web] footer styling

--- web/src/Components/Footer.jsx
+++ web/src/Components/Footer.jsx
@@ -1,12 +1,28 @@
-import React, {Component} from 'react';
-import Search from './Search';
+import React, { Component } from 'react';
 
 class Footer extends Component {
     render() {
         return (
-            <section className="footer">
-                <h3>Footer</h3>
-            </section>
+            <footer className="footer b-0 position-absolute mt-auto py-3 bg-dark text-white">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-4 mt-3 mb-1">
+                            Reserved
+                        </div>
+                        <div className="col-4 mt-3 mb-1">
+                            Reserved
+                        </div>
+                        <div className="col-4 mt-3 mb-1">
+                            Reserved
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 mt-3 mb-1 text-center">
+                            Copyright 2020 camthegeek
+                        </div>
+                    </div>
+                </div>
+            </footer>
         )
     }
 }
