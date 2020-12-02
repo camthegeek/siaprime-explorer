c81d38855deaf71b8ba686254bc43aca67adb0f8
camthegeek <cam@camthegeek.net>
2020-12-02 14:34:52 -0500

    [web] move any api calls to main app area

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,15 +1,70 @@
 import React, {Component} from 'react';
 import './Stylesheets/App.css';
 import Container from "./Components/Container";
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    netdata: [],
+    hashrate: '',
+    totalStorage: '',
+    usedStorage: '',
+    lastblocks: [],
+}
+componentDidMount() {
+    axios.get('http://192.168.1.2:42424/api/health')
+        .then(blockStuff => {
+            let hr = getReadableHashRateString(blockStuff.data.difficulty / blockStuff.data.blockfrequency);
+            let sb = getReadableStorageString(blockStuff.data.totalStorage);
+            let us = getReadableStorageString(blockStuff.data.usedStorage);
+            console.log(blockStuff.data)
+            this.setState({
+                netdata: blockStuff.data,
+                hashrate: hr,
+                totalStorage: sb,
+                usedStorage: us,
+            })
+        }).catch((err) => { 
+            console.log(err);
+        })
+
+        axios.get('http://192.168.1.2:42424/api/last/10')
+        .then(lastBlocks => {
+            this.setState({
+                lastblocks: lastBlocks.data
+            })
+        }).catch((err) => { 
+            console.log(err);
+        })
+}
+
   render() { 
       return (
-    <div className="App">
-      <Container/>
+    <div className="App flex-fill">
+      <Container data={this.state}/>
     </div>
   );
 }
+}
+
+export function getReadableHashRateString(hr) {
+  var i = 0;
+  var byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH'];
+  while (hr > 1000) {
+      hr = hr / 1000;
+      i++;
+  }
+  return hr.toFixed(2) + byteUnits[i];
+}
+
+export function getReadableStorageString(storage) {
+  var i = 0;
+  var byteUnits = [' bytes', ' KB', ' MB', ' GB', ' TB', ' PB'];
+  while (storage > 1024) {
+      storage = storage / 1024;
+      i++;
+  }
+  return storage.toFixed(2) + byteUnits[i];
 }
 
 export default App;
