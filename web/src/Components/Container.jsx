52357ce188e0e58ac7cd7663d05891402dc10398
camthegeek <cam@camthegeek.net>
2020-12-02 10:04:19 -0500

    [web] 404 error handling.

--- web/src/Components/Container.jsx
+++ web/src/Components/Container.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../Pages/Home';
 import BlockInfo from '../Pages/Blocks';
+import NotFound from '../Pages/404';
 
 class Container extends Component {
     render() {
@@ -10,6 +11,7 @@ class Container extends Component {
             <Switch> {/* The Switch decides which component to show based on the current URL.*/}
                 <Route exact path='/' component={Home}></Route>
                 <Route exact path='/block/:id' component={BlockInfo}></Route>
+                <Route component={NotFound} />
             </Switch>
         );
     }
---
+++ web/src/Pages/404.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class NotFound extends Component {
+    render() {
+        return(
+            <section className="404">
+               Uh oh, page not found!
+            </section>
+        )
+    }
+}
+
+export default NotFound;
