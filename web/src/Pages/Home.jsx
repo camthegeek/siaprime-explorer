feaf980af3fa6e8298807428aa6eb037bc7dc4de
camthegeek <cam@camthegeek.net>
2020-12-05 15:07:54 -0500

    [web] add lasttx component to home

--- web/src/Pages/Home.jsx
+++ web/src/Pages/Home.jsx
@@ -1,13 +1,19 @@
 import React, {Component} from "react";
 import NetworkData from '../Components/Network';
 import LastBlocks from '../Components/LastBlocks';
+import LastTx from '../Components/LastTx';
 
 class Home extends Component {
     render() {
         return(
             <section className="container">
                 <NetworkData data={this.props.morestates}/>
+                <div class="container-fluid">
+                    <div className="row">
                 <LastBlocks woot={this.props.morestates}/>
+                <LastTx lasttx={this.props.morestates}/>
+                </div>
+                </div>
             </section>
         )
     }
