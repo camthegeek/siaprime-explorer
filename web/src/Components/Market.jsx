1e91c5393a773dbd114e88be6f1e7fe8109cadfb
camthegeek <cam@camthegeek.net>
2021-01-14 08:01:58 -0500

    [web] markets: fix class tag to className

--- web/src/Components/Market.jsx
+++ web/src/Components/Market.jsx
@@ -19,7 +19,7 @@ class MarketData extends Component {
                                             <Cash className="float-left" size={32}/>
                                         </div>
                                         <div className="media-body ml-3">
-                                            <h4 className="font-weight-bold">ScPrime Price (SCP)</h4> <span>{this.props.data.market.market_data.current_price.usd} (<span class={(this.props.data.market.market_data.price_change_percentage_24h < 0) ? 'text-danger' : 'text-success' }>{this.props.data.market.market_data.price_change_percentage_24h}%</span>) </span>
+                                            <h4 className="font-weight-bold">ScPrime Price (SCP)</h4> <span>{this.props.data.market.market_data.current_price.usd} (<span className={(this.props.data.market.market_data.price_change_percentage_24h < 0) ? 'text-danger' : 'text-success' }>{this.props.data.market.market_data.price_change_percentage_24h}%</span>) </span>
                                         </div>
                                     </div>
                                 </div>
